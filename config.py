10245d8d6ca44cdde67e5f1cb936b1b01fda582a
Federico Vitali <federicovitali1517@gmail.com>
2025-08-18 18:07:39 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,9 @@ HEADERS = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
 
-PRICE_TRESHOLD = {
+PRICE_THRESHOLD = {
     'protein':9.99,
     'creatine':4.99
     }
+
 
