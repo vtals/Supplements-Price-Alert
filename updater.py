885007926db4c23d1e442da3ae7aa606e07efbaf
Federico Vitali <federicovitali1517@gmail.com>
2025-08-18 17:32:03 +0200

    updater.py

--- updater.py
+++ updater.py
@@ -3,14 +3,14 @@ from datetime import datetime
 from config import PROTEIN_URL, CREATINE_URL
 
 def update_protein_data(get_protein_data):
-    protein_df = pd.read_csv('bulk_protein_data.csv')
+    protein_df = pd.read_csv('data/bulk_protein_data.csv')
 
     new_protein_data = get_protein_data(url=PROTEIN_URL)
     protein_df_new = pd.DataFrame(new_protein_data, columns=['flavour', 'weight in g', 'price', 'price_discounted', 'price_currency'])
     protein_df_new['date'] = datetime.now().strftime('%Y-%m-%d')
     
     protein_df_updated = pd.concat([protein_df, protein_df_new], ignore_index=True)
-    protein_df_updated.to_csv('bulk_protein_data.csv', index=False)
+    protein_df_updated.to_csv('data/bulk_protein_data.csv', index=False)
     
     return protein_df_updated
 
@@ -24,6 +24,7 @@ def update_creatine_data(get_creatine_data):
     creatine_df_new['date'] = datetime.now().strftime('%Y-%m-%d')
     
     creatine_df_updated = pd.concat([creatine_df, creatine_df_new], ignore_index=True)
-    creatine_df_updated.to_csv('bulk_creatine_data.csv', index=False)
+    creatine_df_updated.to_csv('data/bulk_creatine_data.csv', index=False)
     
+
     return creatine_df_updated
